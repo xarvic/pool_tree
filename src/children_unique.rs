fd13d317fdf34faa35e36212ec47290cf55ff1ea
Chris <jck2000@gmx.de>
2020-10-25 16:10:03 +0100

    Fixed bugs

--- src/children_unique.rs
+++ src/children_unique.rs
@@ -14,13 +14,11 @@ impl<'a, T: 'static> ChildrenUnique<'a, T> {
         }
     }
     pub fn id(&mut self) -> ChildrenUnique<T> {
-        unsafe {
-            ChildrenUnique{
-                inner: self.inner.id()
-            }
+        ChildrenUnique{
+            inner: self.inner.id()
         }
     }
-    pub fn get_child_uniqe(&mut self, index: u32) -> ChildUniq<'a, T> {
+    pub fn get_child_unique(&mut self, index: u32) -> ChildUniq<T> {
         self.inner.get_child_unique(index)
     }
     pub fn iter(&self) -> ChildIter<T, Ref<T>> {
